a21b924fc94ef9febc4aa94c998e85b1b4651bd5
ooosuke <oosuke@dissmo.biz>
2025-08-30 20:15:47 +0900

    fix typo

--- app/models/quiz_result.rb
+++ app/models/quiz_result.rb
@@ -1,18 +1,18 @@
-class QuizeResult < ApplicationRecord
-  validates :quize, presence: true
+class QuizResult < ApplicationRecord
+  validates :quiz, presence: true
   validates :completion_time, presence: true
   validates :correct_count, presence: true
   validates :answers, presence: true
 
   serialize :answers, JSON
 
-  enum :quize, {
+  enum :quiz, {
     default: 0
   }, prefix: true
 
   def runking
     # 同じクイズの結果を取得し、正解数降順、完了時間昇順でソート
-    same_quiz_results = QuizeResult.where(quize: self.quize)
+    same_quiz_results = QuizResult.where(quiz: self.quize)
                                    .order(correct_count: :desc, completion_time: :asc)
     
     # 自分より上位の結果の数を数える + 1 が順位
@@ -27,7 +27,7 @@ class QuizeResult < ApplicationRecord
   end
 
   def total_participants
-    QuizeResult.where(quize: self.quize).count
+    QuizResult.where(quiz: self.quize).count
   end
 
   def score
---
+++ db/migrate/20250830102256_create_quiz_results.rb
@@ -0,0 +1,16 @@
+class CreateQuizResults < ActiveRecord::Migration[7.1]
+  def change
+    create_table :quiz_results, id: :string, limit: 26 do |t|
+      t.quiz :integer, null: false
+      t.bigint :completion_time, null: false
+      t.integer :correct_count, null: false
+      t.json :answers, null: false
+
+      t.timestamps
+    end
+
+    add_index :quiz_results, :quiz
+    add_index :quiz_results, :completion_time
+    add_index :quiz_results, :correct_count
+  end
+end
